7484ae7a895912872aaa96746dcd07e6a5e5a664
RabiaYasaroglu <60512074+RabiaYasaroglu@users.noreply.github.com>
2021-12-23 19:26:05 +0300

    Add project files.

---
+++ NorthwindProject.API/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindProject.Entity;
+using NorthwindProject.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly Nortwind_DbContext _context;
+        public ProductsController(Nortwind_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public IEnumerable<Products> Get()
+        {
+            var list = _context.products.ToList();
+            return list;
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public Products Get(int id)
+        {
+            return _context.products.FirstOrDefault(x => x.ProductID == id);
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NorthwindProject.Entity/Nortwind_DbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindProject.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindProject.Entity
+{
+    public class Nortwind_DbContext : DbContext
+    {
+        public Nortwind_DbContext(DbContextOptions<Nortwind_DbContext> dbContextOptions)
+           : base(dbContextOptions)
+        {
+
+        }
+
+        public Nortwind_DbContext() { }
+        public DbSet<Categories> categories { get; set; }
+        public DbSet<Products> products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Scans a given assembly for all types that implement IEntityTypeConfiguration, and registers each one automatically
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
